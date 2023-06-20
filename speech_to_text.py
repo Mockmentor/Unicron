0551ca2806d8dca7f0d437ecb0237ec4de1fa0c8
Albert Khazipov <aaooo00aa0@gmail.com>
2023-06-20 16:58:32 +0300

    edit language

--- speech_to_text.py
+++ speech_to_text.py
@@ -3,4 +3,4 @@ import speech_recognition as sr
 def speech_to_text(audio: str) -> str:
     audio_file = audio
     r = sr.Recognizer()
-    return r.recognize_google(audio_file, language="ru-RU")
+    return r.recognize_google(audio_file, language="en-IN")

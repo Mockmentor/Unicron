c87bc896de4d4757c1dab4ff8f616edbcc95fc3a
Dima <15Dnau@gmail.com>
2023-07-17 21:38:03 +0300

    new

--- text_to_speech.py
+++ text_to_speech.py
@@ -5,7 +5,7 @@ from TTS.api import TTS
 tts_model = TTS("tts_models/en/ljspeech/tacotron2-DDC")
 
 
-def tts_to_file(text):
+def text_to_audio(text):
     path = tts_model.tts_to_file(text)
     file = open(path, "rb").read()
     return file
